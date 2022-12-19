1ec4b887855c6290923c74c570d28bfd44ae4673
zyongjun <496280405@qq.com>
2022-12-20 02:38:42 +0800

    generate compat utils

--- app/src/main/java/com/example/myapplication/ITest.java
+++ app/src/main/java/com/example/myapplication/ITest.java
@@ -1,7 +1,4 @@
 package com.example.myapplication;
 
-import com.example.compat.PluginDepended;
-
-@PluginDepended
 public interface ITest {
 }
--- testplugin/src/main/java/com/example/testplugin/PluginActivity.java
+++ testplugin/src/main/java/com/example/testplugin/PluginActivity.java
@@ -4,15 +4,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-import com.example.commsdk.compat.IRecPlugin;
-import com.example.compat.PluginDepended;
-
-import java.lang.reflect.Method;
-import java.util.Arrays;
-import java.util.function.Function;
-import java.util.stream.Collectors;
-
-@PluginDepended
 public class PluginActivity extends AppCompatActivity {
 
     @Override
